def78cb91785ee91db56c443cfb60ed94c144067
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-09 23:00:47 +0500

    Create dFolderToMzxmlConverter.m

---
+++ ToolBox/DataConversion/dFolderToMzxmlConverter.m
@@ -0,0 +1,35 @@
+function mzXMLFullFileName = dFolderToMzxmlConverter(DFolderFullPath,MSConvertOutputResultFolder)
+%SUMMARY: Converting d folder to mzXML file
+% Converting .d folder to mzxml file and will returns the mzxml file path
+
+setenv('MSConvert_PATH', [pwd '..\ProteoWizard']);
+% mustBeTextScalar([pwd '\ProteoWizard\msconvert.exe' ' ' File '-e .mzXML -v --64 -z -o ' ' ' Results])
+[status, ~] = system([pwd '\ProteoWizard\msconvert.exe' ' ' DFolderFullPath ' ' ' --mzXML --v --64 -o ' ' ' MSConvertOutputResultFolder]);
+
+
+if (status == 0)
+
+    mzXMLFullFileName = MSConvertOutputResultFolder
+
+else
+
+    msgbox("File conversion error while converting .d folder to mzxml file.", "File Conversion Error", "error");
+
+end
+
+% % 
+% %     _with compressions_
+% % msconvert mydfoldername.d --mzXML --v --zlib
+% % msconvert mydfoldername.d --mzXML --v --64 --zlib
+% % msconvert mydfoldername.d --mzXML --v --64 --mz64 --inten64 --zlib
+% % 
+% % 
+% % _without compression_
+% % msconvert mydfoldername.d --mzXML --v
+
+
+
+
+
+
+end
