d4e9f600bc81125c592fa02611edc1a90ed37f0f
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-09 23:00:40 +0500

    Update Main.m

--- ToolBox/Main.m
+++ ToolBox/Main.m
@@ -11,15 +11,44 @@ function [] = Main()
 
 %%%% Write a function for local deployment that will create all directories beforehand
 
+clear all
+clc
+%% Setting up paths
+InputFolderPath = "D:\PerceptronXfmsInputFolder";
+mkdir(InputFolderPath);
+
+ResultFolderPath = "D:\PerceptronXfmsResultFolder";
+mkdir(InputFolderPath);
+
+
+%% Sub tools paths
+MSConvertCMDPath = [pwd '\ProteoWizard'];
+SpectrumXfmsPath = [pwd '\SPECTRUM-XFMS_v1.0.0.0'];
+Bridge2Path = [pwd '\Bridge2'];
+
+
+%% HERE HARD CODE THE INPUT FOLDER BUT WILL ASK FROM USER AS UIGET
+
+
 [InFilePath, InFileName, InExt] = fileparts('D:\GitHub\02_WebTool\WebTool\ToolBox\InputTestFile\CheY-100-MS1-r-001.d');     %% For local deployment - SelectDFolder = uigetdir(path,'Select .d Folder');
+
+
+%% HERE HARD CODE THE RESULT FOLDER BUT WILL ASK FROM USER AS UIGET
+
+
 
 %% CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
 if (InExt == '.d')     % CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
 
-    DFolderFullPath = [ InFilePath '\' InFileName '\' InExt];
+    DataConversionPathFolder = "DataConversion\";
+    addpath(DataConversionPathFolder);
+
+    DFolderFullPath = [ InFilePath '\' InFileName InExt];
     MSConvertOutputResultFolder = '.\MSConvertOutputResultFolder';
     % Using MSConvert .d folder to mzXML
     mzXMLFullFileName = dFolderToMzxmlConverter(DFolderFullPath,MSConvertOutputResultFolder);
+
+    rmpath(DataConversionPathFolder);
 
 elseif (InExt == '.mzXML')
 
