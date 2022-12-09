ba680aa5f6ee789e893096462519de3fa7b58c5d
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-09 16:13:19 +0500

    Update readMzXML_Files_V3.R

--- mzXMLtocsvConverter/readMzXML_Files_V3.R
+++ mzXMLtocsvConverter/readMzXML_Files_V3.R
@@ -15,50 +15,99 @@ list_of_files <- list.files(path = pwd, pattern = "\\.mzXML$");
 #list_of_files=list.files(pwd)
 
 length(list_of_files)
-for ( fileNum in 1: length(list_of_files) )
-{
-  FileName= list_of_files[fileNum]
+
+
+Header <- t(c("RT_sec" , "mz" , "Int" ,"RT_min" ))
+#write.csv(Header,NameFile, row.names = FALSE, col.names = FALSE)
+
+CsvFileName = "CheY-100-MS1-r-001 - Test2.csv"
+write.table(Header, CsvFileName, sep = ",", row.names = FALSE, col.names = FALSE, !file.exists(CsvFileName), append = T) 
+
+rm(Data)
+#for ( fileNum in 1: length(list_of_files) )   #UNCOMMENT ME
+
+  FileName = "CheY-100-MS1-r-001 - Test2.mzXML"   #= list_of_files[fileNum]  #UNCOMMENT ME
   mzxml<-readMzXmlFile(FileName, removeMetaData = FALSE, verbose = FALSE)
   
+  TotalScans = length(mzxml)
+  for (NoOfScan in  1:TotalScans)
+  {
+    Masses <- mzxml[[NoOfScan]]$spectrum$mass
+    Intensities <- mzxml[[NoOfScan]]$spectrum$intensity
+    RTsec <- mzxml[[NoOfScan]]$metaData$startTime
+    
+    iter = length(Masses)
+    for(index in 1:iter){
+      MZ <- Masses[index]
+      Int <- Intensities[index]
+      RTmin <- RTsec/60
+      Data <- t(c(RTsec, MZ, Int, RTmin))
+      #write.csv(Data,NameFile, row.names = FALSE, col.names = Header)
+      write.table(Data, CsvFileName, sep = ",", row.names = FALSE, col.names = FALSE, !file.exists(CsvFileName), append = T)
+      rm(Data)
+    }
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   #plot(mzxml[[1]]$spectrum$mass, mzxml[[1]]$spectrum$intensity, type="l")
-  mzxml[1]
+  # mzxml[1]
   ##################################################
   #chromatogram(rtime= mzxml[[1]]$metaData$retentionTime   ,mz=mzxml[[1]]$spectrum$mass, intensity=mzxml[[1]]$spectrum$intensity)
   
-  mz=t(t(mzxml[[1]]$spectrum$mass))
-  Int=t(t(mzxml[[1]]$spectrum$intensity))
+  # mz=t(t(mzxml[[1]]$spectrum$mass))
+  # Int=t(t(mzxml[[1]]$spectrum$intensity))
+  
+  
+ 
+
+  
   
   ##########################
-  # where a is rowIndex of Data
-  a=1
+  # # where a is rowIndex of Data
+  # a=1
+  # 
+  # data<-data.frame()
+  # # Y is from 1 to 6275 i.e. no of spectras in mzxml 
+  # for (y in 1: length (mzxml)) {
+  #   rt=0
+  #   # i is the number of m/z obtain in one spectra 
+  #   for (i in 1: length(mzxml[[y]]$spectrum$mass)) {
+  #     rt[i]=(mzxml[[y]]$metaData$retentionTime)
+  #   }
+  #   rt=t(t(rt))
+  #   data[a:(a+length(mzxml[[y]]$spectrum$mass)-1),1:3]= data.frame( rt=  t(t(rt)) ,mz =c( t(t(mzxml[[y]]$spectrum$mass))),Int=c(t(t(mzxml[[y]]$spectrum$intensity))))
+  #   a=a+length(mzxml[[y]]$spectrum$mass)
+  # }
+  # #################################################
+  # 
+  # for (len in 1 : nrow(data))
+  # {
+  #   data[len,4]= data[len,1]/60
+  # }
+  # 
+  # # WILL USE BELOW
+  # colnames(data)<-c("RT_sec" , "mz" , "Int" ,"RT_min" )
+  # 
+  # DecimalPosition=as.integer(gregexpr(pattern = ".", FileName, fixed = TRUE))
+  # Name=substring(FileName,1,DecimalPosition-1)
+  # NameFile<-paste(Name,'.csv', sep ="")
+  # write.csv(data,NameFile, row.names = FALSE)
+  # rm(data)
   
-  data<-data.frame()
-  # Y is from 1 to 6275 i.e. no of spectras in mzxml 
-  for (y in 1: length (mzxml)) {
-    rt=0
-    # i is the number of m/z obtain in one spectra 
-    for (i in 1: length(mzxml[[y]]$spectrum$mass)) {
-      rt[i]=(mzxml[[y]]$metaData$retentionTime)
-    }
-    rt=t(t(rt))
-    data[a:(a+length(mzxml[[y]]$spectrum$mass)-1),1:3]= data.frame( rt=  t(t(rt)) ,mz =c( t(t(mzxml[[y]]$spectrum$mass))),Int=c(t(t(mzxml[[y]]$spectrum$intensity))))
-    a=a+length(mzxml[[y]]$spectrum$mass)
-  }
-  #################################################
+  # WILL USE ABOVE
   
-  for (len in 1 : nrow(data))
-  {
-    data[len,4]= data[len,1]/60
-  }
-  
-  colnames(data)<-c("RT_sec" , "mz" , "Int" ,"RT_min" )
-  
-  DecimalPosition=as.integer(gregexpr(pattern = ".", FileName, fixed = TRUE))
-  Name=substring(FileName,1,DecimalPosition-1)
-  NameFile<-paste(Name,'.csv', sep ="")
-  write.csv(data,NameFile, row.names = FALSE)
-  rm(data)
-}
+
 #####################
 #data[data$mz >592.333 & data$mz <=592.339 , ]
 #data[data$RT_sec==1.08  , ]
