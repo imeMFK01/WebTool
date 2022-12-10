1405a0318c9678b9a989b2699b6fd4cf32c92f02
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-10 23:45:16 +0500

    Update FetchFileNames.m

--- ToolBox/InputFileOperations/FetchFileNames.m
+++ ToolBox/InputFileOperations/FetchFileNames.m
@@ -1,4 +1,4 @@
-function [DFiles, MzxmlFiles] = FetchFileNames(QueryFullFolderPath, InsideExp, RepNum, DoseResponseInfo)
+function InputFilesInfo = FetchFileNames(QueryFullFolderPath, InsideExp, RepNum, DoseResponseInfo)
 
 
 % This function will fetch the name of the .d &/or mzXML files from the directory 
@@ -6,13 +6,13 @@ function [DFiles, MzxmlFiles] = FetchFileNames(QueryFullFolderPath, InsideExp, R
 % along with their names
 
 
-%% DEL ME
+%% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME
 
 QueryFullFolderPath = "D:\PerceptronXfmsInputFolder\0b284da3-b2ff-481a-9384-fa8fd99961d9";
 RepNum = "Replicate1";
 InsideExp = "\Exp\";
 
-%% DEL ME
+%% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME
 
 
 RepPath = QueryFullFolderPath + InsideExp + RepNum;
@@ -31,29 +31,14 @@ if (exist(RepPath) == 7)  %%  || (exist(Rep1Path) == 0)  %%% Checks only for fol
 
     if size(InputFilesInfo,1) == 0
 
- ME = MException(['FileNotFound:' char(RepNum) ' do not contain any file'], 'Input file not found', 'Invalid input replicate folder');
-
-throw(ME)
+        % Throw error here..
+        %  ME = MException(['FileNotFound:' char(RepNum) ' do not contain any file'], 'Input file not found', 'Invalid input replicate folder');
+        %
+        % throw(ME)
     end
-
-
 end
 
 end
 
-function InputFilesInfo = StructuringFileNames(UnStructFiles)
 
-Name = {UnStructFiles.name};
-FullFolderPath = {UnStructFiles.folder};
-
-Size = size(Name,2);
-InputFilesInfo = strings(Size,2);
-
-for index = 1: Size
-
-InputFilesInfo(index,:) = [Name(index), FullFolderPath(index)];
-
-end
-
-end
 
