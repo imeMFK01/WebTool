d31be28c26bbb8dcba6342a71487fec98669f554
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-10 15:08:22 +0500

    Update FetchFileNames.m

--- ToolBox/InputFileOperations/FetchFileNames.m
+++ ToolBox/InputFileOperations/FetchFileNames.m
@@ -1,8 +1,9 @@
-function [FileNames] = FetchFileNames(QueryFullFolderPath, RepNum, DoseResponseInfo)
+function [DFiles, MzxmlFiles] = FetchFileNames(QueryFullFolderPath, InsideExp, RepNum, DoseResponseInfo)
 
+
+% This function will fetch the name of the .d &/or mzXML files from the directory 
 % This function will check the existance of number of replicates and files
 % along with their names
-
 
 
 %% DEL ME
@@ -19,16 +20,40 @@ if (exist(RepPath) == 7)  %%  || (exist(Rep1Path) == 0)  %%% Checks only for fol
     DFiles = dir(fullfile(RepPath, '*.d'));
     MzxmlFiles = dir(fullfile(RepPath, '*.mzxml'));
 
+    InputFilesInfo = [];
+    if ~isempty({DFiles.name})
+        InputFilesInfo = [InputFilesInfo; StructuringFileNames(DFiles)];
+    end
+
+    if ~isempty({MzxmlFiles.name})
+        InputFilesInfo = [InputFilesInfo; StructuringFileNames(MzxmlFiles)];
+    end
+
+    if size(InputFilesInfo,1) == 0
+
+ ME = MException(['FileNotFound:' char(RepNum) ' do not contain any file'], 'Input file not found', 'Invalid input replicate folder');
+
+throw(ME)
+    end
+
 
 end
 
 end
 
+function InputFilesInfo = StructuringFileNames(UnStructFiles)
 
+Name = {UnStructFiles.name};
+FullFolderPath = {UnStructFiles.folder};
 
-function [] = CompareDoseAndFileName()
+Size = size(Name,2);
+InputFilesInfo = strings(Size,2);
 
+for index = 1: Size
 
-
+InputFilesInfo(index,:) = [Name(index), FullFolderPath(index)];
 
 end
+
+end
+
