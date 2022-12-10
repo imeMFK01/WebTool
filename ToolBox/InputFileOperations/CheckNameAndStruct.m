00b56112b434cd146a3d30fddd8c4bf0abf233c1
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-10 14:08:43 +0500

    Update CheckNameAndStruct.m

--- ToolBox/InputFileOperations/CheckNameAndStruct.m
+++ ToolBox/InputFileOperations/CheckNameAndStruct.m
@@ -1,4 +1,4 @@
-function Test = CheckNameAndStruct(MainFullFolderPath)
+function Test = CheckNameAndStruct(QueryFullFolderPath, DoseResponseFile)
 
 
 % This function will check the names of the input files/folders and the directory's structure
@@ -6,16 +6,31 @@ function Test = CheckNameAndStruct(MainFullFolderPath)
 % This function will check the Number of Replicates, number and type of doses
 
 % Number and type of doses in each replicate should be equal otherwise
-% THROUGH ERROR
+% THROW ERROR
 
-listing = dir(MainFullFolderPath)
+
+
+DoseResponseInfoTable = readtable(DoseResponseFile, 'Format', '%s', 'ReadVariableNames',false);
+DoseResponseInfo = DoseResponseInfoTable(:,1);
+
+
+
+listing = dir(QueryFullFolderPath)
+
+
+[FileNames] = FetchFileNames(QueryFullFolderPath, RepNum, DoseResponseInfo)
 
 
 DFullFileNames = [];
-[] = FetchFileNames(MainFullFolderPath, "Replicate1");
-[] = FetchFileNames(MainFullFolderPath, "Replicate2");
-[] = FetchFileNames(MainFullFolderPath, "Replicate3");
+[] = FetchFileNames(QueryFullFolderPath, "Replicate1");
+[] = FetchFileNames(QueryFullFolderPath, "Replicate2");
+[] = FetchFileNames(QueryFullFolderPath, "Replicate3");
 
+
+
+%
+
+if ()
 
 
 end
