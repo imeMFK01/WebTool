dfe56622dbe9cf64a055611316bcbd43bf1d5718
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-10 14:08:47 +0500

    Update FetchFileNames.m

--- ToolBox/InputFileOperations/FetchFileNames.m
+++ ToolBox/InputFileOperations/FetchFileNames.m
@@ -1,15 +1,34 @@
-function [] = FetchFileNames(MainFullFolderPath, RepNum)
+function [FileNames] = FetchFileNames(QueryFullFolderPath, RepNum, DoseResponseInfo)
 
 % This function will check the existance of number of replicates and files
 % along with their names
 
-RepPath = MainFullFolderPath + "\" + RepNum;
+
+
+%% DEL ME
+
+QueryFullFolderPath = "D:\PerceptronXfmsInputFolder\0b284da3-b2ff-481a-9384-fa8fd99961d9";
+RepNum = "Replicate1";
+InsideExp = "\Exp\";
+
+%% DEL ME
+
+
+RepPath = QueryFullFolderPath + InsideExp + RepNum;
 if (exist(RepPath) == 7)  %%  || (exist(Rep1Path) == 0)  %%% Checks only for folders.
     DFiles = dir(fullfile(RepPath, '*.d'));
     MzxmlFiles = dir(fullfile(RepPath, '*.mzxml'));
 
-    DFullFileNames = [DFullFileNames; string(DFiles.name)]
 
 end
+
+end
+
+
+
+function [] = CompareDoseAndFileName()
+
+
+
 
 end
