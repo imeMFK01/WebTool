561688b809f05edd39e0d27fce852c904cc7ad7d
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-11 20:05:40 +0500

    Create ConversionIntoMzxml.m

---
+++ ToolBox/FileFormatConverters/ConversionIntoMzxml.m
@@ -0,0 +1,51 @@
+function [] = ConversionIntoMzxml(InputFilesData, MSConvertCMDPath, MainProcessingFolder)
+
+
+
+
+for index = 1: size(InputFilesData,1)
+
+    mzXMLFileOutputDir = MainProcessingFolder + "\" + InputFilesData(index,5);
+
+    if InputFilesData(index,3) == ".d"
+
+        mzXMLFullFileName = CallMSConvertCMD(MSConvertCMDPath, InputFilesData(index,:), mzXMLFileOutputDir)
+
+    end
+
+
+
+
+end
+
+
+
+
+
+
+%% CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
+if (InExt == '.d')     % CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
+
+   
+
+    %FARHAN - Here should go the list of .d folders with full path
+    DFolderFullPath = [ InFilePath '\' InFileName InExt];
+    MSConvertOutputResultFolder = '.\MSConvertOutputResultFolder';
+    % Using MSConvert .d folder to mzXML
+    mzXMLFullFileName = dFolderToMzxmlConverter(MSConvertCMDPath, DFolderFullPath,MSConvertOutputResultFolder);
+
+    
+
+elseif (InExt == '.mzXML')
+
+        mzXMLFullFileName = [ InFilePath '\' InFileName '\' InExt];
+
+else
+
+    msgbox("File format is incompatible. Please either use .d folder or .mzXML file for computations.", "File Format Not Supported", "error");
+
+end
+
+
+
+end
