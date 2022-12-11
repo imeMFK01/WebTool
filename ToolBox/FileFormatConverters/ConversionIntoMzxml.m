3a0e75a99b084dfaf33184fbcab0c2d1d40840f0
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-11 20:45:30 +0500

    Update ConversionIntoMzxml.m

--- ToolBox/FileFormatConverters/ConversionIntoMzxml.m
+++ ToolBox/FileFormatConverters/ConversionIntoMzxml.m
@@ -1,7 +1,7 @@
 function [] = ConversionIntoMzxml(InputFilesData, MSConvertCMDPath, MainProcessingFolder)
 
 
-
+mzXMLFilesInfo = strings(size(InputFilesData,1),2);
 
 for index = 1: size(InputFilesData,1)
 
@@ -9,7 +9,9 @@ for index = 1: size(InputFilesData,1)
 
     if InputFilesData(index,3) == ".d"
 
-        mzXMLFullFileName = CallMSConvertCMD(MSConvertCMDPath, InputFilesData(index,:), mzXMLFileOutputDir)
+        CallMSConvertCMD(MSConvertCMDPath, InputFilesData(index,:), mzXMLFileOutputDir);
+
+        
 
     end
 
@@ -20,32 +22,32 @@ end
 
 
 
-
-
-
-%% CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
-if (InExt == '.d')     % CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
-
-   
-
-    %FARHAN - Here should go the list of .d folders with full path
-    DFolderFullPath = [ InFilePath '\' InFileName InExt];
-    MSConvertOutputResultFolder = '.\MSConvertOutputResultFolder';
-    % Using MSConvert .d folder to mzXML
-    mzXMLFullFileName = dFolderToMzxmlConverter(MSConvertCMDPath, DFolderFullPath,MSConvertOutputResultFolder);
-
-    
-
-elseif (InExt == '.mzXML')
-
-        mzXMLFullFileName = [ InFilePath '\' InFileName '\' InExt];
-
-else
-
-    msgbox("File format is incompatible. Please either use .d folder or .mzXML file for computations.", "File Format Not Supported", "error");
-
-end
-
+% 
+% 
+% 
+% %% CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
+% if (InExt == '.d')     % CONVERTING .D FOLDER TO MZXML FILE AND WILL RETURNS THE FILE PATH
+% 
+%    
+% 
+%     %FARHAN - Here should go the list of .d folders with full path
+%     DFolderFullPath = [ InFilePath '\' InFileName InExt];
+%     MSConvertOutputResultFolder = '.\MSConvertOutputResultFolder';
+%     % Using MSConvert .d folder to mzXML
+%     mzXMLFullFileName = dFolderToMzxmlConverter(MSConvertCMDPath, DFolderFullPath,MSConvertOutputResultFolder);
+% 
+%     
+% 
+% elseif (InExt == '.mzXML')
+% 
+%         mzXMLFullFileName = [ InFilePath '\' InFileName '\' InExt];
+% 
+% else
+% 
+%     msgbox("File format is incompatible. Please either use .d folder or .mzXML file for computations.", "File Format Not Supported", "error");
+% 
+% end
+% 
 
 
 end
