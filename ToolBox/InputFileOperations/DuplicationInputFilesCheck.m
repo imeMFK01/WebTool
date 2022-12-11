97cf5c0dff30c56b416ef0d32b99ebee3742a0b1
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-11 17:13:12 +0500

    Update DuplicationInputFilesCheck.m

--- ToolBox/InputFileOperations/DuplicationInputFilesCheck.m
+++ ToolBox/InputFileOperations/DuplicationInputFilesCheck.m
@@ -1,4 +1,4 @@
-function [] = DuplicationInputFilesCheck(InputFilesInfo)
+function [] = DuplicationInputFilesCheck(InputFilesInfo, RepNum)
 %Check if user provided two different format files of same dose then throw
 %error
 
@@ -8,6 +8,10 @@ OnlyFileNameUnique = unique(OnlyFileName);
 
 if size(OnlyFileName,1) ~= size(OnlyFileNameUnique,1)
     % throw(ME)
+    % Dear User, your input query cannot be processed because you have
+    % provided two different files (.d & .mzXML) against a single dose in Replicate 'X'
+    % (RepNum). So, please provide single file against each dose.
+
 end
 
 end
