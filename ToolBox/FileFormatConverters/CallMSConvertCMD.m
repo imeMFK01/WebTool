62a092ab74a163ffe0b167a7610972c31793709b
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-11 20:45:33 +0500

    Update CallMSConvertCMD.m

--- ToolBox/FileFormatConverters/CallMSConvertCMD.m
+++ ToolBox/FileFormatConverters/CallMSConvertCMD.m
@@ -1,14 +1,26 @@
-function mzXMLFullFileName = CallMSConvertCMD(MSConvertCMDPath, DFolderInfo, mzXMLFileOutputDir)
+function [] = CallMSConvertCMD(MSConvertCMDPath, DFolderInfo, mzXMLFileOutputDir)
 
+% %% PLACEHOLDERS FOR TESTING  %% PLACEHOLDERS FOR TESTING  %% PLACEHOLDERS FOR TESTING
+% DFolderInfo =  InputFilesData(index,:);
+% 
+% 
+% %% PLACEHOLDERS FOR TESTING  %% PLACEHOLDERS FOR TESTING  %% PLACEHOLDERS FOR TESTING
 
 
 %SUMMARY: Converting d folder to mzXML file
 % Converting .d folder to mzxml file and will returns the mzxml file path
 
+DFolderFullPath = char(DFolderInfo(1,4) + "\" + DFolderInfo(1,2));
+
+%Just for safety purpose otherwise not needed - first deleting mzXML file if
+%already exists and then creates a new one.
+delete(mzXMLFileOutputDir+ "\" + DFolderInfo(1,1) + ".mzXML");
+
 setenv('MSConvert_PATH', MSConvertCMDPath);
 % mustBeTextScalar([pwd '\ProteoWizard\msconvert.exe' ' ' File '-e .mzXML -v --64 -z -o ' ' ' Results])
-[status, ~] = system([MSConvertCMDPath '\msconvert.exe' ' ' DFolderInfo(1,4) ' ' ' --mzXML --v --64 -o ' ' ' mzXMLFileOutputDir]);
+[status, cmdout] = system([MSConvertCMDPath '\msconvert.exe' ' ' DFolderFullPath ' ' ' --mzXML --v --64 -o ' ' ' char(mzXMLFileOutputDir)]);
 
+%status == 0 means command completed successfully
 
 if (status ~= 0)
 
@@ -16,6 +28,7 @@ if (status ~= 0)
     %Dear User, your input .d file of Replicate 'X' [[ DFolderInfo(1,4) ]] dose'Y' [[  DFolderInfo(1,1)  ]]  has been corrupted
     %therefore, we are unable to process your query further. So, please provide the
     %correct (uncorrupted) file.
+    % #DevUse - print [[  cmdout  ]] error 
     %     msgbox("File conversion error while converting .d folder to mzxml file.", "File Conversion Error", "error");
 
 end
