681b519e0c41ec617a40bc31ad8ade52d6330f74
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-16 01:29:38 +0500

    Update ChangingCsvLocAndNames.m

--- ToolBox/Utilities/ChangingCsvLocAndNames.m
+++ ToolBox/Utilities/ChangingCsvLocAndNames.m
@@ -1,4 +1,4 @@
-function CsvFilesInfo = ChangingCsvLocAndNames(mzXMLFilesInfo, CsvPathBeforeFilter)
+function [] = ChangingCsvLocAndNames(mzXMLFilesInfo, ReplaceStringFrom, ReplaceStringWith)
 %%Code for replacing the filenames based on current Comparison Engine
 %%conventions i.e. Dose-100-MS1.csv
 
@@ -9,31 +9,36 @@ EndString = "-MS1";
 CsvFilesInfo = mzXMLFilesInfo;
 
 
-NewCsvNameVector = extractBefore(mzXMLFilesInfo(:,3), '.mzXML');
+
 OldCsvNameVector = strcat(extractBefore(mzXMLFilesInfo(:,3), '.mzXML'), '.csv');
 
 
-movefile(OldCsvNameVector(:,1),'newname.csv');
 
-CsvFilesInfo(:,3) = strcat(NewCsvNameVector, '.csv')
 
 
 strcat(StartString, extractBetween(CsvFilesInfo(:,3),'Dose', '.mzXML'), EndString, ".csv");
 
 
+for index = 1: size(mzXMLFilesInfo,1)
 
 
+SourceCsvName = strcat(mzXMLFilesInfo(index,2), '\' ,OldCsvNameVector(index,1));
+DestinationCsvName = strrep(mzXMLFilesInfo(index,2), ReplaceStringFrom, ReplaceStringWith);
+mkdir(DestinationCsvName);
 
-
-
-for index = 1: size(CsvFilesInfo,1)
-
-    if(mzXMLFilesInfo(index,3))
-
+movefile(SourceCsvName, DestinationCsvName);
+end
 
 
 
 end
+% 
+% source = "D:\PerceptronXfmsIntermediateProcessingFolder\0b284da3-b2ff-481a-9384-fa8fd99961d9\Exp\Replicate1\Dose0.mzXML"
+% 
+% destination = "D:\PerceptronXfmsIntermediateProcessingFolder\0b284da3-b2ff-481a-9384-fa8fd99961d9\Input\Replicate1"
+% mkdir(destination)
+% movefile(source, destination)
 
-end
 
+
+NewCsvNameVector = extractBefore(mzXMLFilesInfo(:,3), '.mzXML');
