70d947530d747e3a71071a9f40ebf601e230d463
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-16 01:05:24 +0500

    Update IntermediateProcessingDir.m

--- ToolBox/Utilities/IntermediateProcessingDir.m
+++ ToolBox/Utilities/IntermediateProcessingDir.m
@@ -1,4 +1,4 @@
-function MainProcessingFolder = IntermediateProcessingDir(IntermediateProcessingFolderPath, GUID, RepArr)
+function MainProcessingFolder = IntermediateProcessingDir(MainProcessingDir, InsideExp, RepArr)
 
 % %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME
 % GUID = "0b284da3-b2ff-481a-9384-fa8fd99961d9";
@@ -7,12 +7,12 @@ function MainProcessingFolder = IntermediateProcessingDir(IntermediateProcessing
 % %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME   %% DEL ME
 
 
-MainProcessingFolder = IntermediateProcessingFolderPath + "\" + GUID;
+MainProcessingFolder = MainProcessingDir + InsideExp;
 
 mkdir(MainProcessingFolder);
 
 for index = 1: size(RepArr,1)
-RepPath = MainProcessingFolder + "\" + RepArr(index);
+RepPath = MainProcessingFolder + RepArr(index);
 mkdir(RepPath);
 end
 
