ced4e450863eace1331a73b2bc4bc19ac4607012
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-26 08:26:07 +0500

    Firebase ApiKey Updated

--- PerceptronXfmsFrontEnd/src/environments/environment.ts
+++ PerceptronXfmsFrontEnd/src/environments/environment.ts
@@ -6,16 +6,26 @@
 export const environment = {
   production: false,
   firebase: {
-apiKey: "AIzaSyCqHOjPtqY7MxWRtj-RDW7aI-n8fe1ldmM",
-    authDomain: "perceptron-51a86.firebaseapp.com",
-    databaseURL: "https://perceptron-51a86.firebaseio.com",
-    projectId: "perceptron-51a86",
-    storageBucket: "perceptron-51a86.appspot.com",
-    messagingSenderId: "708562387352",
-    appId: "1:708562387352:web:f685ab2e4474c6c941abed",
-    measurementId: "G-318PE2YSNP"
+    apiKey: "AIzaSyCC_1YxtoTMJcgBpJSxfrZICypWQy0CucI",
+  authDomain: "perceptron-xfms.firebaseapp.com",
+  projectId: "perceptron-xfms",
+  storageBucket: "perceptron-xfms.appspot.com",
+  messagingSenderId: "956090883975",
+  appId: "1:956090883975:web:d1f1e9460e66d43712bd22"
+
   }
 };
+
+
+// apiKey: "AIzaSyCqHOjPtqY7MxWRtj-RDW7aI-n8fe1ldmM",
+//     authDomain: "perceptron-51a86.firebaseapp.com",
+//     databaseURL: "https://perceptron-51a86.firebaseio.com",
+//     projectId: "perceptron-51a86",
+//     storageBucket: "perceptron-51a86.appspot.com",
+//     messagingSenderId: "708562387352",
+//     appId: "1:708562387352:web:f685ab2e4474c6c941abed",
+//     measurementId: "G-318PE2YSNP"
+
 
 
 
