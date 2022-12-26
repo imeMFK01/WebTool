89bcc3fd9ecc2c2a59d7801bc7a0c5fe7affd136
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-26 10:49:59 +0500

    Update WebApiConfig.cs

--- PerceptronXfmsAPI/PerceptronXfmsAPI/App_Start/WebApiConfig.cs
+++ PerceptronXfmsAPI/PerceptronXfmsAPI/App_Start/WebApiConfig.cs
@@ -21,40 +21,40 @@ namespace PerceptronXfmsAPI
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            //config.Filters.Add(new RequireHttpsAttribute());  //THIS IS COMMENTED JUST FOR...
-            
-            
+            //config.Filters.Add(new RequireHttpsAttribute());  //Uncomment this for using https://localhost:44300/
+
+
             //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("multipart/form-data"));
 
-           // config.Routes.MapHttpRoute(
-           //    name: "loginApi",
-           //    routeTemplate: "api/{controller}/{action}/{id}/{pass}/{g}"
-           // );
+            // config.Routes.MapHttpRoute(
+            //    name: "loginApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}/{pass}/{g}"
+            // );
 
 
-           // config.Routes.MapHttpRoute(
-           //    name: "activateApi",
-           //    routeTemplate: "api/{controller}/{action}/{em}"
-           // );
+            // config.Routes.MapHttpRoute(
+            //    name: "activateApi",
+            //    routeTemplate: "api/{controller}/{action}/{em}"
+            // );
 
 
 
-           // config.Routes.MapHttpRoute(
-           //    name: "RegisterApi",
-           //    //routeTemplate: "api/{controller}/{action}/{UName}/{Name}/{Email}/{R_Pass}"
-           //    routeTemplate: "api/{controller}/{action}/{id}/{pass}/{g}/{k}"
-           // );
+            // config.Routes.MapHttpRoute(
+            //    name: "RegisterApi",
+            //    //routeTemplate: "api/{controller}/{action}/{UName}/{Name}/{Email}/{R_Pass}"
+            //    routeTemplate: "api/{controller}/{action}/{id}/{pass}/{g}/{k}"
+            // );
 
-           // config.Routes.MapHttpRoute(
-           //   name: "LogoutApi",
-           //   routeTemplate: "api/{controller}/{action}/{id}/{g}"
-           //);
+            // config.Routes.MapHttpRoute(
+            //   name: "LogoutApi",
+            //   routeTemplate: "api/{controller}/{action}/{id}/{g}"
+            //);
 
 
-           // config.Routes.MapHttpRoute(
-           //   name: "updatePassApi",
-           //   routeTemplate: "api/{controller}/{action}/{ID}/{newPass}"
-           //);
+            // config.Routes.MapHttpRoute(
+            //   name: "updatePassApi",
+            //   routeTemplate: "api/{controller}/{action}/{ID}/{newPass}"
+            //);
 
 
 
@@ -62,7 +62,7 @@ namespace PerceptronXfmsAPI
             //   name: "updateInfoApi",
             //   routeTemplate: "api/{controller}/{action}/{ID}/{newName}/{newEmail}"
             //);
-            
+
             config.Routes.MapHttpRoute(
               name: "querryApi",
               routeTemplate: "api/{controller}/{action}"
