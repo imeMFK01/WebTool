0faf617e98f5ae4970bc650c7274880b8d42a6bc
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2022-12-28 02:12:17 +0500

    Added

---
+++ PerceptronXfmsSimulationService/PerceptronXfmsSimulationService/DTO/Call2MATLABDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronXfmsSimulationService.DTO
+{
+    public class Call2MATLABDto
+    {
+        public string QueryResultFullPath;
+        public string Error;
+        public string ErrorLog;
+    }
+}
