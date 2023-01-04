c5d8598c1c445c7eacd831498daf29f9895bb3e6
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2023-01-04 17:37:17 +0500

    Update app.component.ts

--- PerceptronXfmsFrontEnd/src/app/app.component.ts
+++ PerceptronXfmsFrontEnd/src/app/app.component.ts
@@ -196,15 +196,23 @@ export class AppComponent {
     };
   };
   contact() {
-    var user = firebase.auth().currentUser;
 
-    if (user) {
+    var user = firebase.auth().currentUser;
+    if (user != null) {
       this.UserEmailID = user.email;
-      this.router.navigate(['/contact']);
-    } else {
-      alert("Dear User,\nPlease login via PERCEPTRON-XFMS user account for performing protein search or click \"Login as Guest\" to proceed without creating PERCEPTRON-XFMS user account.\n\nThank you for your interest in PERCEPTRON-XFMS!\nThe PERCEPTRON-XFMS Team.")
-      this.router.navigate(['/login']);
-    };
+    }
+    this.router.navigate(['/contact']);
+
+
+    // var user = firebase.auth().currentUser;
+
+    // if (user) {
+    //   this.UserEmailID = user.email;
+    //   this.router.navigate(['/contact']);
+    // } else {
+    //   alert("Dear User,\nPlease login via PERCEPTRON-XFMS user account for performing protein search or click \"Login as Guest\" to proceed without creating PERCEPTRON-XFMS user account.\n\nThank you for your interest in PERCEPTRON-XFMS!\nThe PERCEPTRON-XFMS Team.")
+    //   this.router.navigate(['/login']);
+    // };
   }
 
   adminpanel() {
