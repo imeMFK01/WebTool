8fe6eadb168d7c6e197d37ccfeb4956189b8e802
Muhammad Farhan Khalid <farhan.biomedical@gmail.com>
2023-02-23 21:23:57 +0500

    Update mzXMLtocsvConverter.R

--- ToolBox/FileFormatConverters/mzXMLtocsvConverter.R
+++ ToolBox/FileFormatConverters/mzXMLtocsvConverter.R
@@ -18,7 +18,7 @@ library(readMzXmlData)
 library(jsonlite)
 
 
-MzxmlFullFileNamePath = "D:\\PerceptronXfmsIntermediateProcessingFolder\\11000000-0000-0000-0000-000000000000\\Exp\\Replicate3\\Dose75.mzXML"
+MzxmlFullFileNamePath = "D:\\PerceptronXfmsIntermediateProcessingFolder\\3b0dce0f-fe9a-4b73-a2a3-6a82ba611a26\\Exp\\Replicate1\\Dose0.mzXML"
 
 Name = tools::file_path_sans_ext(MzxmlFullFileNamePath)
 CsvFileName<-paste(Name,".csv", sep ="")
@@ -30,61 +30,3 @@ OutputFileFullName<-paste(OutputFileName,".txt",sep ="")
 fileConn<-file(  OutputFileFullName)
 writeLines(JsonString, fileConn)
 close(fileConn)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
